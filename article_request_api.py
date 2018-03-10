a7b95875a72fe62e575512815924489baab7646d
Ben Madany <benjamin.madany@gmail.com>
2018-03-09 19:16:15 -0800

    Wraps up the initial version of the wikipedia request wrapper. Updates article request api to use wiki requests methods.

--- article_request_api.py
+++ article_request_api.py
@@ -1,25 +1,20 @@
 # Detailed Wikipedia Skill
 # Author: Ben Madany
 
-import wikipedia as wiki
+import wiki_requests as wiki
 from traceback import format_exc
 
 
 def request_article(article):
     try:
-        top_result = wiki.search(article, 1)[0]
-        print("Retrieving suggested article: " + str(top_result))
-        summary = wiki.summary(top_result)
-        print("Retrieved summary")
-        page = wiki.page(top_result)
-        print("Retrieved page data")
-        # TODO: modify wiki request api to retrieve section headings
-        for category in page.categories:
-            print(category)
-    except wiki.DisambiguationError as err:
-        print(err)
-        return False
-    except wiki.PageError as err:
+        title = wiki.prefix_search(article)
+        print("Retrieving suggested article: " + str(title))
+        categories = wiki.parse_sections(title)
+        print("Retrieving article categories:")
+        for category in categories:
+            subheading, index = category
+            print(subheading + " at " + str(index))
+    except wiki.GenericWikipediaException as err:
         print(err)
         return False
     except:
@@ -27,13 +22,15 @@ def request_article(article):
         return False
     return True
 
-def request_section(article, section):
+def request_section(article, section=None):
     try:
-        top_result = wiki.search(article, 1)[0]
-        print ("Retrieving suggested article: " + str(top_result))
-        page = wiki.page(top_result)
-        print("Retrieved page data")
-        # TODO: modify wiki request api to retrieve section summaries
+        title = wiki.prefix_search(article)
+        print ("Retrieving suggested article: " + str(title))
+        content = wiki.get_content(title, section=section)
+        print("Retrieved content:\n" + content)
+    except wiki.GenericWikipediaException as err:
+        print(err)
+        return False
     except:
         print(format_exc())
         return False
--- wiki_requests.py
+++ wiki_requests.py
@@ -1,68 +1,143 @@
 # Detailed Wikipedia Skill
 # Author: Ben Madany
 
+
+from HTMLParser import HTMLParser
 import requests
+import re
 
 
+# Wikipedia API endpoint, starting language code determines which Wiki to use
 API_URL = "http://en.wikipedia.org/w/api.php"
+# User Agent that must be specified in headers of requests sent to Wikipedia API
 USER_AGENT = "Detailed Wikipedia Skill (https://github.com/benmadany/detailed-wikipedia-skill)"
 
 
-class WikipediaException(msg):
-    # TODO
+# Generic Exception for errors returned by Wikipedia API
+class GenericWikipediaException(Exception):
+
+    def __init__(self, error):
+        self.error = error
+
+    def __unicode__(self):
+        return "The following error occured: {0}".format(self.error)
+
+    def __str__(self):
+        return self.__unicode__().encode('utf8')
 
 
+# Logging wrapper
 def log_msg(msg, error=0):
-    print(msg)
+    print("Error:\n------------\n" if error else "Info:\n------------\n" + msg + "\n------------")
 
 
+# Utility method to strip extra newlines from text, remove references and links (text surrounded by '[]' or lines beginning with '^'), and remove lines that do not end with '.'
+def clean_text(text):
+    text = re.sub('(\[.*\])|(^\^.*$)|(^.[^\.]*$)', '', text, flags=re.MULTILINE)
+    text = re.sub('(\n{2,})', '\n', text)
+    return text
+
+
+# Creates dict containing common parameters for the Wikipedia API requests that will be made
 def default_params():
-    params = {"format":"json"}
+    params = {'format':'json', 'redirects':1}
     return params
 
 
+# Creates dict containing common headers for any Wikipedia API requests
 def default_headers():
     global USER_AGENT
-    headers = {"User Agent": USER_AGENT}
+    headers = {'User-Agent': USER_AGENT}
     return headers
 
 
-# Request prefix search of a specific article, returns a tuple containing the title and pageid of the first result (Wikipedia's suggestion)
+# Request prefix search of a specific article, returns the title of the first result (Wikipedia's suggestion)
 def prefix_search(pssearch):
     params = default_params()
-    params["action"] = "query"
-    params["list"] = "prefixsearch"
-    params["pssearch"] = pssearch
+    params['action'] = 'query'
+    params['list'] = 'prefixsearch'
+    params['pssearch'] = pssearch
 
     results_json = send_request(params)
 
-    if 'error' in results_json
-        if results_json['error']['info'] in ('HTTP request timed out.', 'Pool queue is full'):
-            raise HTTPTimeoutError(pssearch)
-        else:
-            raise WikipediaException(results_json['error']['info'])
+    if 'error' in results_json:
+        raise GenericWikipediaException(results_json['error']['info'])
 
-    top_result_id = results_json['query']['prefixsearch'][0]['title'], results_json['query']['prefixsearch'][0]['pageid']
+    top_result = results_json['query']['prefixsearch'][0]['title']
 
-    return top_result_id
+    return top_result
 
 
-def parse_sections(pageid):
+# Request parse action with the 'sections' property specified, returns a list of tuples containing all the sub-headings and their indexes of an article
+def parse_sections(page):
     params = default_params()
-    params["action"] = "parse"
-    params["pageid"] = pageid
-    params["prop"] = "sections"
+    params['action'] = 'parse'
+    params['page'] = page
+    params['prop'] = 'sections'
 
-    send_request(params)
+    results_json = send_request(params)
+
+    if 'error' in results_json:
+        raise GenericWikipediaException(results_json['error']['info'])
+
+    sections = ((section['line'], section['index']) for section in results_json['parse']['sections'])
+
+    return list(sections)
 
 
-def parse_content(pageid, section=None, count=0):
-    params = {}
+# Request content of a page summary or a section, returns parsed and cleaned text from Wikipedia
+def get_content(page, section=None, count=0):
+    params = default_params()
+    if section is not None:
+        params['action'] = 'parse'
+        params['page'] = page
+        params['section'] = section
+        params['prop'] = 'text'
+    else:
+        params['action'] = 'query'
+        params['titles'] = page
+        params['prop'] = 'extracts'
+        params['exintro'] = 1
+
+    results_json = send_request(params)
+
+    if 'error' in results_json:
+        raise GenericWikipediaException(results_json['error']['info'])
+
+    content = ""
+    if section is not None:
+        content = strip_tags(results_json['parse']['text']['*'])
+    else:
+        content = strip_tags(results_json['query']['pages'][results_json['query']['pages'].keys()[0]]['extract'])
+
+    return clean_text(content)
 
 
+# Sends http request to Wikipedia API returning a dictionary of the retrieved content in json format
 def send_request(params):
     headers = default_headers()
 
     global API_URL
+    log_msg("Sending request to: " + API_URL + "\nParameters: " + str(params) + "\nHeaders: " + str(headers))
     results = requests.get(API_URL, params=params, headers=headers)
-    return results.json()
+    results_json = results.json()
+    log_msg("Received: " + str(results) + "\n" + str(results_json))
+    return results_json
+
+
+# Class and method to strip html tags from text, from StackOverflow: https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+# Class and method to strip html tags from text, from StackOverflow: https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
